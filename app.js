997426be47727ef2e8db37afa82926032f49b0e8
Rafik <rafikbahri.insat@gmail.com>
2018-06-21 20:23:09 +0100

    Production Rounting React - Express

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser=require('body-parser');
 const keys = require('./config/keys');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 Days in millisec
@@ -20,7 +19,7 @@ app.use(passport.session());
 
 
 mongoose.connect(keys.mongoURI).then(res => {
-    console.log('CONNECTED TO MLAB DEV DATABASE');
+    console.log('CONNECTED TO MLAB DEV DATABASE');O
 });
 
 //Use body-parser middleware
@@ -35,5 +34,20 @@ app.get('/', (req, res) => {
     res.send('Surveys APP');
 });
 
+//Configure routing (react-express) in production mode
+if(process.env.NODE_ENV==="production"){
+    //Express will serve up production assets
+    //like main.js, main.css files..
+    app.use(express.static('client/build'));
+
+    //Express will serve up the index.html file
+    //if it doesn't recognize the route
+    const path=require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    });
+
+}
+const PORT = process.env.PORT || 5000;
 console.log("SERVER LISTENING ON PORT: " + PORT);
 app.listen(PORT);
